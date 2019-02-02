3c789ceacae2c8455c240058c11cf8bb6e9ac676
Alexander Strakhovenko <sasstr@gmail.com>
2019-02-03 01:38:36 +0300

    Внес правки XSS

--- templates/index.php
+++ templates/index.php
@@ -5,7 +5,7 @@
             <!--заполните этот список из массива категорий-->
             <?php foreach ($categories as $val): ?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?= $val ?></a>
+                <a class="promo__link" href="pages/all-lots.html"><?= htmlspecialchars($val); ?></a>
             </li>
             <?php endforeach ?>
         </ul>
@@ -19,15 +19,15 @@
             <?php foreach ($lots as $val): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?= $val['url'] ?>" width="350" height="260" alt="">
+                    <img src="<?= htmlspecialchars($val['url']); ?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?= $val['category'] ?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $val['title'] ?></a></h3>
+                    <span class="lot__category"><?= htmlspecialchars($val['category']); ?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($val['title']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?= format_price ($val['price']) ?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?= format_price(htmlspecialchars($val['price'])) ?><!-- <b class="rub">р</b> --></span>
                         </div>
                         <div class="lot__timer timer">
                             <?= $time_until_midnight ?>
