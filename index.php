5dc8c5e71192ba4746a0c5cde0987b3f17b59289
Alexander Strakhovenko <sasstr@gmail.com>
2019-02-06 01:12:58 +0300

    Добавил сценарий регистрации на сайте пользователя SQL

--- index.php
+++ index.php
@@ -3,11 +3,24 @@ require_once('config.php');
 require_once('functions.php');
 require_once('data.php');
 
-/* $link = mysqli_connect(DB['HOST'], DB['LOGIN'], DB['PASSWORD'], DB['NAME']);
+/* // Регистрация пользователя на сайте
+$link = mysqli_connect(DB['HOST'], DB['LOGIN'], DB['PASSWORD'], DB['NAME']);
 if ($link == false) {
     print("Ошибка подключения: " . mysqli_connect_error());
 };
 mysqli_set_charset($link, 'utf8');
+
+$email = mysqli_real_escape_string($link, $_POST['email']); // Так для всех полей надо делать?
+$password = password_hash($_POST['password']);
+$name = $_POST['name'];
+$contacts = $_POST['message'];
+$avatar = $_POST['']; // avatar - нет поля name в разметке
+
+$sql = INSERT INTO users (email, password, name, contacts, avatar) VALUES (?, ?, ?, ?, ?); // Запрос к БД
+$stmt = mysqli_prepare($link, $sql);
+mysqli_stmt_bind_param($stmt, 'ssssb', $email, $password, $name, $contacts, $avatar);
+mysqli_stmt_execute($stmt);
+
 */
 
 $index_page = render('index', $major_indexes);
