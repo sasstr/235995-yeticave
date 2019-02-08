acd817412da6e65ecc0067265a7c07d4548febf3
Alexander Strakhovenko <sasstr@gmail.com>
2019-02-08 23:37:14 +0300

    Поправил запросы SQL на добавление данных в таблицы

--- queries.sql
+++ queries.sql
@@ -7,14 +7,14 @@ VALUES
   ('Инструменты'),
   ('Разное');
 
-INSERT INTO lots(`title`, `description`, `url_image`, `starting_price`, `starting_date`, `bet_step`)
+INSERT INTO lots(`title`, `description`, `url_image`, `starting_price`, `starting_date`, `bet_step`, `user_id`, `winner_id`)
 VALUES
-('2014 Rossignol District Snowboard', NULL, 'img/lot-1.jpg', 10999, 700),
-('DC Ply Mens 2016/2017 Snowboard', NULL, 'img/lot-2.jpg', 159999, 2000),
-('Крепления Union Contact Pro 2015 года размер L/XL', NULL, 'img/lot-3.jpg', 8000, 500),
-('Ботинки для сноуборда DC Mutiny Charocal', NULL, 'img/lot-4.jpg', 10999, 800),
-('Куртка для сноуборда DC Mutiny Charocal', NULL, 'img/lot-5.jpg', 7500, 300),
-('Маска Oakley Canopy', NULL, 'img/lot-6.jpg', 5400, 200);
+('2014 Rossignol District Snowboard', 'настоящий сноуборд', 'img/lot-1.jpg', 10999, '2019-01-01 21:15:10', 700, 1, 1),
+('DC Ply Mens 2016/2017 Snowboard', 'самая быстрая доска', 'img/lot-2.jpg', 159999, '2019-02-03 10:45:10', 2000, 2, 2),
+('Крепления Union Contact Pro 2015 года размер L/XL', 'отличные крепления', 'img/lot-3.jpg', 8000, '2019-01-06 12:55:10',  500, 3, 3),
+('Ботинки для сноуборда DC Mutiny Charocal', 'крутые ботинки для сноуборда', 'img/lot-4.jpg', 10999, '2019-01-12 11:35:10', 800, 4, 4),
+('Куртка для сноуборда DC Mutiny Charocal', 'хайповый куртец для сноубордирования', 'img/lot-5.jpg', 7500, '2019-01-09 21:25:10', 300, 5, 5),
+('Маска Oakley Canopy', 'маска на все лицо', 'img/lot-6.jpg', 5400, '2019-01-25 09:05:10', 200, 6, 6);
 
 INSERT INTO users(`email`, `password`, `name`, `contacts`, `avatar`)
 VALUES
