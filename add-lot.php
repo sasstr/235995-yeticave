6a316aab7074646f29b64ac4bd5bc48c6750f4dd
Alexander Strakhovenko <sasstr@gmail.com>
2019-02-23 14:28:26 +0300

    WIP Эксперементы

--- add-lot.php
+++ add-lot.php
@@ -17,7 +17,8 @@ define( 'INSERT_NEW_LOT',
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $new_lot = $_POST;
-    var_dump($new_lot );
+    var_dump($new_lot);
+    var_dump($_FILES);
     $required_fields = ['lot-name', 'category', 'message', 'lot-rate', 'lot-step', 'lot-date'];
     $errors = [];
 
@@ -64,23 +65,37 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $file_path = __DIR__ . '/upload/';
             $file_url = '/upload/' . trim($file_name_uniq);
             move_uploaded_file($file_tmp_name, $file_path . $file_name_uniq);
-            $sql = 'INSERT INTO lots (`title`, `description`, `img-file`, `starting_price`, `rate_step`, `finishing_date` `category_id`, `author_id`)
-                    VALUES (?, ?, ?, ?, ?, ?, ?, ?);';
-            $stmt = db_get_prepare_stmt($link, $sql, [$new_lot['lot-name'],
+            /* $sql = 'INSERT INTO lots (`title`, `description`, `img-file`, `starting_price`, `rate_step`, `finishing_date` `category_id`, `author_id`)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?);'; */
+            $author_id = 7;
+            /* $stmt = db_get_prepare_stmt($link, $sql, [$_POST['lot-name'],
+
+            $_POST['message'],
+                                                    $file_url,
+                                                    $_POST['lot-rate'],
+                                                    $_POST['lot-step'],
+                                                    $_POST['lot-date'],
+                                                    $_POST['category'],
+                                                    $author_id
+                                                    ]); */
+            $sql = 'INSERT INTO lots
+            (`title`, `description`, `img_path`, `starting_price`, `starting_date`, `rate_step`, `finishing_date`, `user_id`, `winner_id`, `category_id`)
+            VALUE ('2019 Rossi Snowboard New', 'настоящий крутой сноуборд', 'img/lot-1.jpg', 8790, '2019-02-01 11:15:10', 700, '2019-05-08 21:15:10', 7, 7, 1);';
+            /* $stmt = mysqli_prepare($link, $sql); var_dump($stmt);*/
+            /*  mysqli_stmt_bind_param($stmt, 'sssssssi', [$new_lot['lot-name'],
                                                     $new_lot['message'],
                                                     $file_url,
                                                     $new_lot['lot-rate'],
                                                     $new_lot['lot-step'],
                                                     $new_lot['lot-date'],
-                                                    $new_lot['category'],
-                                                    7]);
-            /*$stmt = mysqli_prepare($link, $sql);
-             mysqli_stmt_bind_param($stmt, 'sssssssi', ); */
-            /*  */
+                                                    $new_lot['category']
+                                                    ]); */
+
             $res = mysqli_stmt_execute($stmt);
             if ($res) {
                 $lot_id = mysqli_insert_id($link);
                 header("Location: lot.php?id=" . $lot_id);
+                exit();
             } else {
                 $errors['form'] = 'Пожалуйста, исправьте ошибки в форме.';
                 $add_lot = render('add', [
