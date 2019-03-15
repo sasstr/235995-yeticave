44f71abb66df592184fd7272c83b0150c531ee8d
Alexander Strakhovenko <sasstr@gmail.com>
2019-03-16 01:41:45 +0300

    WIP отправка письма победителю

--- templates/getwinner.php
+++ templates/getwinner.php
@@ -12,4 +12,21 @@ function db_get_lots_not_winners($link) {
     return [];
 };
 
+/* $transport = new Swift_SmtpTransport('smtp.example.org', 25);
+// Формирование сообщения
+$message = new Swift_Message("Просмотры вашей гифки");
+$message->setTo(["keks@htmlacademy.ru" => "Кекс"]);
+$message->setBody("Вашу гифку «Кот и пылесос» посмотрело больше 1 млн!");
+$message->setFrom("mail@giftube.academy", "GifTube");
+// Отправка сообщения
+$mailer = new Swift_Mailer($transport);
+$mailer->send($message);
 
+Остальные параметры перечислены ниже:
+Имя параметра	Значение
+Тема письма	Ваша ставка победила
+Отправитель	keks@phpdemo.ru
+Получатель	E-mail пользователя-победителя
+Content-type тела письма	text/html
+
+*/
